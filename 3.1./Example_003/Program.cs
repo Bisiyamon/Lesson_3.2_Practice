6922c8fa9a44b51a7e881923ee3c19163a1ef07e
Дмитрий Бардаков <bardphotograf@gmail.com>
2023-02-12 13:58:58 +0300

    My first version Example_003

---
+++ 3.1./Example_003/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Write("Enter a day of week: ");
+int DayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+if(DayOfWeek == 1)
+{
+    Console.WriteLine("This number of day of the week relevants to Monday");
+}
+
+if(DayOfWeek == 2)
+{
+    Console.WriteLine("This number of day of the week relevants to Tuesday");
+}
+
+if(DayOfWeek == 3)
+{
+    Console.WriteLine("This number of day of the week relevants to Wednesday");
+}
+
+if(DayOfWeek == 4)
+{
+    Console.WriteLine("This number of day of the week relevants to Thursday");
+}
+
+if(DayOfWeek == 5)
+{
+    Console.WriteLine("This number of day of the week relevants to Friday");
+}
+
+if(DayOfWeek == 6)
+{
+    Console.WriteLine("This number of day of the week relevants to Saturday");
+}
+
+if(DayOfWeek == 7)
+{
+    Console.WriteLine("This number of day of the week relevants to Sunday");
+}
