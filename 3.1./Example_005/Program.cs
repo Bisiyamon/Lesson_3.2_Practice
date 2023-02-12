1b89914df0ac9d59cf788982a6b773693d51ddef
Дмитрий Бардаков <bardphotograf@gmail.com>
2023-02-13 00:09:21 +0300

    The first version Example_005 - Last digit

---
+++ 3.1./Example_005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int remains = number%10;
+if(number > 99 & number <1000)
+{
+    Console.Write("The last digit in your number = ");
+    Console.WriteLine(remains);
+}
+else
+{
+    Console.WriteLine("Read carefully, you need input three-digit number");
+}
