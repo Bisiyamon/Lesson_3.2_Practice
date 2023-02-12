f4f0dd4c28b5ca4841ffaddbd95723cac288e73e
Дмитрий Бардаков <bardphotograf@gmail.com>
2023-02-12 23:14:30 +0300

    The first version Example_004 from -N to N

---
+++ 3.1./Example_004/Program.cs
@@ -0,0 +1,12 @@
+﻿//Программа, которая на вход принимает одно число N на выходе получаем ряд от -N до N
+Console.Write("Enter a number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int reverse = -a;
+int count=reverse;
+
+while(count<=a)
+{
+    Console.Write(count);
+    Console.Write(" / ");
+    count=count+1;
+}
