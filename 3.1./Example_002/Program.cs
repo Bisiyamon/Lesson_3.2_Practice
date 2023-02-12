ed52febdee46e9147b8e8909230cc1b4c176925f
Дмитрий Бардаков <bardphotograf@gmail.com>
2023-02-12 13:32:06 +0300

    My first version Example_002

---
+++ 3.1./Example_002/Program.cs
@@ -0,0 +1,14 @@
+﻿ Console.Write("Input number A: ");
+ double a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number B: ");
+ double b = Convert.ToInt32(Console.ReadLine());
+ double c = a/b;
+
+if (c == b)
+{
+    Console.WriteLine("Number B is radical of Number A");
+}
+else 
+{
+    Console.WriteLine("Number B isn't radical of Number A");
+}
