18b110e039ddf5fb6992f47d41d89635cd6a8e45
Дмитрий Бардаков <bardphotograf@gmail.com>
2023-02-12 09:34:51 +0300

    The first step Example_001

---
+++ 3.1./Example_001/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Input number: ");
+//Console.ReadLine();
+int a = Convert.ToInt32(Console.ReadLine());
+//int a = 0;
+int result = 0;
+result = a * a;
+Console.Write("Result = ");
+Console.WriteLine(result);
