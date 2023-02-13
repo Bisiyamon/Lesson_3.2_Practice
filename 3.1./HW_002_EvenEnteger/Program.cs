1999232cfadef3c1468bde8743820542b4c84320
Дмитрий Бардаков <bardphotograf@gmail.com>
2023-02-14 00:12:21 +0300

    Home work task 3

---
+++ 3.1./HW_002_EvenEnteger/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Please enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number%2 == 0)
+{
+    Console.WriteLine("It's even number");
+}
+else
+{
+    Console.WriteLine("It isn't even number");
+}
