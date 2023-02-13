99521a8ae34ffcf746ab1537582cf8c5c2518dd0
Дмитрий Бардаков <bardphotograf@gmail.com>
2023-02-13 23:50:34 +0300

    Home work task 2

---
+++ 3.1./HW_001_Comparison_2_number/Program.cs
@@ -0,0 +1,58 @@
+﻿Console.Write("Input amout varriables: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void FillArray(int[] numbers)
+{
+    int index = 0;
+    while (index < n)
+    {
+        numbers[index] = Convert.ToInt32(Console.ReadLine());
+        //index = index + 1;
+        index++;
+    }
+}
+
+void PrintArray(int[] PrintNumber)
+{
+    int position = 0;
+    while(position < n)
+    {
+        Console.Write(" / ");
+        Console.Write(PrintNumber[position]);
+        //position = position + 1;
+        position++;
+    }
+}
+
+int Comparison(int[] number) 
+{
+    int index = 0;
+    int max = 0;
+
+    while(index < n)
+    {
+        if(number[index] > max)
+        {
+            max = number[index];
+        }
+        index++;
+    }
+    return max;
+} 
+
+int[] array = new int[n];
+
+Console.WriteLine("Enter numbers");
+FillArray(array);
+Console.WriteLine();
+
+Console.Write("Your Array: ");
+PrintArray(array);
+
+Console.WriteLine();
+Console.WriteLine();
+
+int maxnumber = Comparison(array);
+Console.Write("Max number in your array: ");
+Console.WriteLine(maxnumber);
+Console.WriteLine();
