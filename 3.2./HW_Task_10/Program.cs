f79623a2b19639e8269b831ceca1bfc4e50b0196
Дмитрий Бардаков <bardphotograf@gmail.com>
2023-02-15 15:00:18 +0300

    Home work Task 10

---
+++ 3.2./HW_Task_10/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Input your number: ");
+string num = Console.ReadLine();
+
+char[] symbols = num.ToCharArray();
+
+foreach (char ch in symbols)
+{
+    Console.Write(" / ");
+    Console.Write(ch);
+}
+Console.WriteLine(" /");
+
+Console.WriteLine();
+Console.Write("Please specify the number of position of the digit to be showed: ");
+int input_position = Convert.ToInt32(Console.ReadLine());
+int position = input_position-1;
+
+for(int i=0; i<symbols.Length; i++)
+{
+    if (i == position)
+    {
+        Console.Write("Do you want to see this digit: {0}", symbols[i]);
+    }
+  
+}
+Console.WriteLine();
